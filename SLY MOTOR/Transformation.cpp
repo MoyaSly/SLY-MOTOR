337a136e4f5cc64e7db8b65cb3f8aded6531176c
Miquel Moya Sanjuán <miquelmoyasan@hotmail.com>
2016-10-14 18:25:47 +0200

    external error gg

--- SLY MOTOR/Transformation.cpp
+++ SLY MOTOR/Transformation.cpp
@@ -1,28 +1,63 @@
 #include "Globals.h"
 #include "Transformation.h"
 #include "GameObject.h"
+#include "MathGeoLib/MathBuildConfig.h"
+#include "MathGeoLib/MathGeoLib.h"
+
 
 Transformation::Transformation(GameObject* _object, int _id) : Component (_object, _id)
 {
-	rotation = Quat::identity;
+	/*rotation = Quat::identity;
 	position = float3::zero;
-	scale.Set(1.f, 1.f, 1.f);
+	scale.Set(1.f, 1.f, 1.f);*/
 }
 Transformation::~Transformation()
 {}
 
-Quat Transformation::SetRotation(float3 rot)
+//POSITION
+void Transformation::SetPosition(float3 pos) { position = pos; }
+float3 Transformation::GetPosition() { return position; }
+void Transformation::ResetPosition() { position = float3::zero; }
+
+//ROTATION
+void Transformation::SetRotation(float3 rot)
 {
-	Quat trans = rotation;
-	return trans;
+	if (rot.x < 0)
+		rot.x += 360;
+	if (rot.y < 0)
+		rot.y += 360;
+	if (rot.z < 0)
+		rot.z += 360;
+
+	rot *= DEGTORAD;
+	
+	rotation = Quat::FromEulerXYZ(rot.x, rot.y, rot.z);
 }
-float3 Transformation::SetMove(float3 pos)
+
+float3 Transformation::GetRotation()
 {
-	float3 trans = position;
-	return trans;
+	float3 rot = rotation.ToEulerXYZ();
+
+	rot * RADTODEG;
+
+	if (rot.x < 0)
+		rot.x += 360;
+	if (rot.y < 0)
+		rot.y += 360;
+	if (rot.z < 0)
+		rot.z += 360;
+
+	return rot;
 }
-float3 Transformation::SetScale(float3 scal)
-{
-	float3 trans = scale;
-	return trans;
+
+void Transformation::ResetRotation() { SetRotation(float3::zero); }
+
+
+//SCALE
+void Transformation::SetScale(float3 scal) 
+{ 
+	if (scal.x != 0 && scal.y != 0 && scal.z != 0) 
+		scale = scal;
 }
+float3 Transformation::GetScale() { return scale; }
+void Transformation::ResetScale() { scale.Set(1.f, 1.f, 1.f); }
--- SLY MOTOR/Transformation.h
+++ SLY MOTOR/Transformation.h
@@ -1,9 +1,7 @@
 #ifndef __Transformation_H__
 #define __Transformation_H__
 
-#include "Globals.h"
 #include "Component.h"
-#include "MathGeoLib\MathGeoLib.h"
 
 class Transformation : public Component
 {
@@ -11,16 +9,26 @@ public:
 	Transformation(GameObject* _object, int _id);
 	~Transformation();
 
-	Quat rotation;
+	//POSITION
+	void SetPosition(float3 pos);
+	float3 GetPosition();
+	void ResetPosition();
+
+	//ROTATION
+	void SetRotation(float3 rot);
+	float3 GetRotation();
+	void ResetRotation();
+
+	//SCALE
+	void SetScale(float3 scal);
+	float3 GetScale();
+	void ResetScale();
+
+
+private:
 	float3 position;
+	Quat rotation;
 	float3 scale;
-
-	Quat SetRotation(float3 rot);
-
-	float3 SetMove(float3 pos);
-	float3 SetScale(float3 scal);
-
-
 
 };
 
